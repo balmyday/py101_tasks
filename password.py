f7cfd0abbb97ed4f8be9de8ab78739ba162fbf0e
Anastasiia <isvidanka@gmail.com>
2020-11-08 00:47:07 +0100

    adding solution to the password

--- password.py
+++ password.py
@@ -5,6 +5,15 @@
 Сложным считается пароль, состоящий как минимум из 8-ми символов,
 включая цифры и алфавитные символы
 """
+def has_alpha_and_digit(pswd):
+    return any(c.isalpha() for c in pswd) and any(c.isdigit() for c in pswd)
 
 if __name__ == '__main__':
-    pass
+  
+	password = input()
+	if len(password) < 8:
+	    print("Password is too easy")
+	elif not has_alpha_and_digit(password):
+	    print("Password is too easy")
+	else:
+	    print("ok")

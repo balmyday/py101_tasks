02272879b4747c627ee48052b44f3e3342d22546
Anastasiia <isvidanka@gmail.com>
2020-11-08 00:51:25 +0100

    adding solution to the guess_num

--- guess_num.py
+++ guess_num.py
@@ -13,6 +13,31 @@
 
 Тебе может понадобится модуль random, цикл while и ветвления
 """
+import random
 
 if __name__ == '__main__':
-    pass
+	min_v = 1
+	max_v = 10000000
+	num = random.randint(min_v, max_v)
+	print(f'Hello! Guess a number from {min_v} to {max_v}.')
+	while True:
+	    user_input = input('Enter your guess: ')
+	    if 'exit' in user_input or len(user_input) == 0:
+	        print('looser')
+	        break 
+	    if not user_input.isdigit():
+	        print('It\'s not a number,looser. Try again')
+	        continue
+	    user_number = int(user_input)
+	    if user_number < min_v or user_number > max_v:
+	        print('Your number is out of range. Try again') 
+	        continue
+	    if user_number > num:
+	        print('Your number is greater than mine')
+	    if user_number < num:    
+	        print('Your number is less than mine')
+	    if user_number == num: 
+	        print('You won!')
+	        break
+
+
